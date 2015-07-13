bd6bb12037162bb681a4f023d5c38b66115d8fa6
HaimRaz <haimraitsev@gmail.com>
2015-07-13 21:11:49 +0300

    register location
    register location

--- startbootstrap-modern-business-1.0.3/startbootstrap-modern-business-1.0.3/LocationModule/location.js
+++ startbootstrap-modern-business-1.0.3/startbootstrap-modern-business-1.0.3/LocationModule/location.js
@@ -4,14 +4,15 @@ angular.module('locationModule', [])
             restrict: 'E',
             replace: true,
             scope: {
-                address: '='
+                user: '='
             },
-            template: '<input id="address" name="address" type="text" class="form-control input-lg" placeholder="Address" required/>',
+            template: '<input id="address" name="address" type="text" class="form-control input-lg" placeholder="Address"/>',
             link: function ($scope, elm, attrs) {
                 var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
                 google.maps.event.addListener(autocomplete, 'place_changed', function () {
                     var place = autocomplete.getPlace();
-                    $scope.address = place;
+                    $scope.user = "";
+                    $scope.user.address = place.formatted_address;
                     $scope.$apply();
                 });
             }
--- startbootstrap-modern-business-1.0.3/startbootstrap-modern-business-1.0.3/RegisterModule/registerCtrl.js
+++ startbootstrap-modern-business-1.0.3/startbootstrap-modern-business-1.0.3/RegisterModule/registerCtrl.js
@@ -1,11 +1,13 @@
 recApp.controller('registerCtrl', ['$scope', 'registerService', function ($scope, registerService) {
-    $scope.user = new Object();
-
+//    $scope.user = new Object();
+//    $scope.user.address="";
     $scope.submitForm = function (isValid) {
 
         // check to make sure the form is completely valid
         if (isValid) {
             //Todo go to factory
+            
+            alert(angular.toJson($scope.user));
         }
 
     };

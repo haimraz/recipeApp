46c5c7a8dafe82bfe6f92a2ba63745a0f16bf5c6
HaimRaz <haimraitsev@gmail.com>
2015-07-17 00:10:26 +0300

    web socket ui

--- Client/BigRecipeModule/bigRecipeCtrl.js
+++ Client/BigRecipeModule/bigRecipeCtrl.js
@@ -1,26 +1,17 @@
 recApp.controller('bigRecipeCtrl', ['$scope', 'bigRecipeService', function ($scope, bigRecipeService) {
     $scope.currMsg = {};
-    $scope.currMsg.name = user.name;
-    $scope.messages = [
-        {
-            time: "15",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            name: "Haim"
-            },
-        {
-            time: "10",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            name: "Dudu"
-            },
-        {
-            time: "7",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            name: "Talya"
-            }
-        ];
+    bigRecipeService.passScope($scope);
+    bigRecipeService.loadAllMessages();
+
+    // $scope.messages = bigRecipeService.scope.;
+    bigRecipeService.testWebSocket();
 
     $scope.addComment = function () {
         $scope.currMsg.time = "1";
-        $scope.messages.push(angular.copy($scope.currMsg));
+        $scope.currMsg.name = user.name;
+        var msgCopy = angular.copy($scope.currMsg);
+        //  $scope.messages.push(msgCopy);
+        bigRecipeService.doSend(angular.toJson(msgCopy));
+        $scope.currMsg = {};
     }
 }]);
--- Client/BigRecipeModule/bigRecipeFac.js
+++ Client/BigRecipeModule/bigRecipeFac.js
@@ -1,55 +1,82 @@
 recApp.factory('bigRecipeService', [function () {
 
     var Service = {};
-//    var wsUri = "ws://echo.websocket.org/"; // todo put you socket url
-//
-//    function testWebSocket() {
-//        websocket = new WebSocket(wsUri);
-//        websocket.onopen = function (evt) {
-//            onOpen(evt)
-//        };
-//        websocket.onclose = function (evt) {
-//            onClose(evt)
-//        };
-//        websocket.onmessage = function (evt) {
-//            onMessage(evt)
-//        };
-//        websocket.onerror = function (evt) {
-//            onError(evt)
-//        };
-//    }
-//
-//    function onOpen(evt) {
-//        writeToScreen("CONNECTED");
-//        doSend("WebSocket rocks");
-//    }
-//
-//    function onClose(evt) {
-//        writeToScreen("DISCONNECTED");
-//    }
-//
-//    function onMessage(evt) {
-//        writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
-//        websocket.close();
-//    }
-//
-//    function onError(evt) {
-//        writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-//    }
-//
-//    function doSend(message) {
-//        writeToScreen("SENT: " + message);
-//        websocket.send(message);
-//    }
-//
-//    function writeToScreen(message) {
-//        var pre = document.createElement("p");
-//        pre.style.wordWrap = "break-word";
-//        pre.innerHTML = message;
-//        output.appendChild(pre);
-//    }
-//
-//    window.addEventListener("load", init, false);
+    Service.passScope = function (scope) {
+        Service.scope = scope;
+    }
+    Service.loadAllMessages = function () {
+        Service.scope.messages = [
+            {
+                time: "15",
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                name: "Haim"
+            },
+            {
+                time: "10",
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                name: "Dudu"
+            },
+            {
+                time: "7",
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                name: "Talya"
+            }
+        ];
+    }
+    var wsUri = "ws://echo.websocket.org/"; // todo put you socket url
+
+    Service.testWebSocket = function () {
+        websocket = new WebSocket(wsUri);
+        websocket.onopen = function (evt) {
+            onOpen(evt)
+        };
+        websocket.onclose = function (evt) {
+            onClose(evt)
+        };
+        websocket.onmessage = function (evt) {
+            onMessage(evt)
+        };
+        websocket.onerror = function (evt) {
+            onError(evt)
+        };
+    }
+
+
+    function onOpen(evt) {
+        writeToScreen("CONNECTED");
+        //  Service.doSend("WebSocket rocks");
+    }
+
+    function onClose(evt) {
+        writeToScreen("DISCONNECTED");
+    }
+
+    function onMessage(evt) {
+        writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
+       Service.scope.messages.push(JSON.parse(evt.data));
+        Service.scope.$apply();
+        // angular.$apply();
+        //  websocket.close();
+    }
+
+    function onError(evt) {
+        writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+    }
+
+    Service.doSend = function (message) {
+        writeToScreen("SENT: " + message);
+        websocket.send(message);
+    }
+
+    function writeToScreen(message) {
+        //        var pre = document.createElement("p");
+        //        pre.style.wordWrap = "break-word";
+        //        pre.innerHTML = message;
+        //        output.appendChild(pre);
+        console.log(message);
+    }
+
+    //  window.addEventListener("load", testWebSocket, false);
 
     return Service;
 

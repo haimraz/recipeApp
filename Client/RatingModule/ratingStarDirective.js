6ea954d302360701228d81a7ce62879cb80045a9
HaimRaz <haimraitsev@gmail.com>
2015-07-19 21:13:01 +0300

    merge problems 2

--- Client/RatingModule/ratingStarDirective.js
+++ Client/RatingModule/ratingStarDirective.js
@@ -1,5 +1,4 @@
 angular.module('ratingStarModule', []).
-<<<<<<< HEAD
 factory('ratingService', ['$rootScope', '$http', function ($rootScope, $http)
     {
         var fac = {};
@@ -8,31 +7,6 @@ factory('ratingService', ['$rootScope', '$http', function ($rootScope, $http)
             
         }
         return fac;
-=======
-controller('ctrl', ['$scope', function ($scope) {
-            $scope.rankChange = function (value) {
-                $scope.rank = value;
-                console.log($scope);
-
-            };
-            $scope.stars = [
-                {
-                    id: "star-1"
-                    },
-                {
-                    id: "star-2"
-                    },
-                {
-                    id: "star-3"
-                    },
-                {
-                    id: "star-4"
-                    },
-                {
-                    id: "star-5"
-                    },
-                ];
->>>>>>> 672b75325e3e42e83e3ec792fab6c52f0a2c8a0e
     }]).
 directive('ratingStarDirective', ['userService', function (userService) {
     return {

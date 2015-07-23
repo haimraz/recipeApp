834abf8627549b6d4fc19709b8d9c02918dcc0a2
HaimRaz <haimraitsev@gmail.com>
2015-07-24 01:32:31 +0300

    lital ein li koh latmunot

--- Client/GraphModule/barModule/barFac.js
+++ Client/GraphModule/barModule/barFac.js
@@ -1,6 +1,10 @@
 recApp.factory('barService', ['$http', '$rootScope' , function($http, $rootScope){
      
     var fac = {};
-
+    
+    fac.findInServer = function (query) {
+        return $http.put($rootScope.url + '/Statistics/getAverageRankByCuisine' , query);
+    }
+    
     return fac;
 }]);

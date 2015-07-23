4792f769740112ba79565a8b9025af7f22d5dae1
Ilya Shkil <ildorn@gmail.com>
2015-07-23 23:48:25 +0300

    finished server development by requirements

--- Server/Controllers/Users.js
+++ Server/Controllers/Users.js
@@ -35,7 +35,7 @@ function authenticate(name, pass, fn) {
 }
 
 function restrict(req, res, next) {
-    if (req.session.user) {
+    if (Utils.currentUser(req)) {
         next();
     }
     else {
@@ -45,29 +45,33 @@ function restrict(req, res, next) {
 }
 
 exports.login = function (req, res) {
-    authenticate(req.body.username, req.body.password, function (err, user) {
-        if (user != null) {
-            // Regenerate session when signing in
-            // to prevent fixation
-            req.session.regenerate(function () {
-                // Store the user's primary key
-                // in the session store to be retrieved
-                req.session.user = user;
-                console.log("Connected -> ", req.session.user);
-                Utils.generateResponse(req, res, 1, user.username);
-            });
-        }
-        else {
-            Utils.generateResponse(req, res, 0, err.message);
-        }
-    });
+    if (Utils.currentUser(req)) {
+        Utils.generateResponse(req, res, 0, "User already connected");
+    }
+    else {
+        authenticate(req.body.username, req.body.password, function (err, user) {
+            if (user != null) {
+                // Regenerate session when signing in
+                // to prevent fixation
+                req.session.regenerate(function () {
+                    // Store the user's primary key
+                    // in the session store to be retrieved
+                    req.session.user = user;
+                    console.log("Connected -> ", req.session.user);
+                    Utils.generateResponse(req, res, 1, user.username);
+                });
+            }
+            else {
+                Utils.generateResponse(req, res, 0, err.message);
+            }
+        });
+    }
 };
 
 exports.logout = function (req, res) {
-    var user = req.session.user;
-    if (user != null) {
+    if (Utils.currentUser(req)) {
         req.session.destroy(function () {
-            Utils.generateResponse(req, res, 1, "");
+            Utils.generateResponse(req, res, 1, "User disconnected");
         });
     }
     else {
@@ -76,8 +80,8 @@ exports.logout = function (req, res) {
 };
 
 exports.getCurrentUser = function (req, res) {
-    var user = req.session.user;
-    if (user != null) {
+    var user = Utils.currentUser(req);
+    if (user) {
         Utils.generateResponse(req, res, 1, user.username);
     }
     else {
@@ -86,47 +90,50 @@ exports.getCurrentUser = function (req, res) {
 };
 
 exports.signup = function (req, res) {
-    console.log(req.body.username);
-    console.log(req.body.password);
-    User.find({username: req.body.username}, function (err, userFromDatabase) {
-        console.log(req.body);
+    if (Utils.currentUser(req)) {
+        Utils.generateResponse(req, res, 0, "User already connected");
+    }
+    else {
+        User.find({username: req.body.username}, function (err, userFromDatabase) {
+            console.log(req.body);
 
-        if (err) {
-            Utils.generateResponse(req, res, 0, err.message);
-        }
-        else if (userFromDatabase[0] == null) {
-            if ((req.body.password.toString().length < 11) && (req.body.password.toString().length > 4 )) {
-                console.log("Got user: ", userFromDatabase);
-                var randomSalt = Math.floor(Math.random() * 9000) + 1000;
+            if (err) {
+                Utils.generateResponse(req, res, 0, err.message);
+            }
+            else if (userFromDatabase[0] == null) {
+                if ((req.body.password.toString().length < 11) && (req.body.password.toString().length > 4 )) {
+                    console.log("Got user: ", userFromDatabase);
+                    var randomSalt = Math.floor(Math.random() * 9000) + 1000;
 
-                Utils.hash(req.body.password, randomSalt.toString(), function (err, hash) {
-                    var newUser = new User({
-                        _id: GLOBAL.DB.Types.ObjectId(),
-                        username: req.body.username,
-                        password: hash.toString('hex'),
-                        creation_date: new Date,
-                        email: req.body.email,
-                        address: req.body.address,
-                        salt: randomSalt
+                    Utils.hash(req.body.password, randomSalt.toString(), function (err, hash) {
+                        var newUser = new User({
+                            _id: GLOBAL.DB.Types.ObjectId(),
+                            username: req.body.username,
+                            password: hash.toString('hex'),
+                            creation_date: new Date,
+                            email: req.body.email,
+                            address: req.body.address,
+                            salt: randomSalt
+                        });
+
+                        newUser.save(function (err) {
+                            if (err) {
+                                Utils.generateResponse(req, res, 0, err.message);
+                                console.log(err);
+                            }
+                            else {
+                                Utils.generateResponse(req, res, 1, "");
+                            }
+                        });
                     });
-
-                    newUser.save(function (err) {
-                        if (err) {
-                            Utils.generateResponse(req, res, 0, err.message);
-                            console.log(err);
-                        }
-                        else {
-                            Utils.generateResponse(req, res, 1, "");
-                        }
-                    });
-                });
+                }
+                else
+                    Utils.generateResponse(req, res, 0, "invalid password length. length should be between 5-10 characters");
             }
             else
-                Utils.generateResponse(req, res, 0, "invalid password length. length should be between 5-10 characters");
-        }
-        else
-            Utils.generateResponse(req, res, 0, "username already exist");
-    });
+                Utils.generateResponse(req, res, 0, "username already exist");
+        });
+    }
 };
 
 exports.checkIfUserExist = function (req, res) {
